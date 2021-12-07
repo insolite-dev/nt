c2ca323b05cca96e252bfbb14ea778297fa9f5fa
Ismael Shakverdiev <st.bahahd@gmail.com>
2021-12-07 22:11:21 +0400

    Add create/delete file function utilities
    - Added create function to create or over-write exiting file by given
      body.
    - Added delete function to remove file or folder by just it's name.

--- pkg/utils.go
+++ pkg/utils.go
@@ -15,3 +15,23 @@ func NotyaPWD() (*string, error) {
 
 	return &path, nil
 }
+
+// CreateFile, creates new file and writes to its data.
+func CreateFile(path string, body []byte) error {
+	err := os.WriteFile(path, body, 0o600)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Delete, removes file or folder, from given path.
+func Delete(path string) error {
+	err := os.Remove(path)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
