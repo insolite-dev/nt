751654221814f759460cb81631bf4df03e6bb8a8
Ismael Shakverdiev <st.bahahd@gmail.com>
2021-12-07 17:22:02 +0400

    Add tests for logger
    - Modified logger appropriate to logger
    - Added full-coverage tests for logger

--- pkg/logger.go
+++ pkg/logger.go
@@ -19,15 +19,15 @@ var (
 	Stderr = colorable.NewColorableStderr()
 )
 
-// level is a custom type of `string-level`.
-// used define level for [outputLevel] function.S
-type level string
+// Level is a custom type of `string-level`.
+// used define level for [OutputLevel] function.
+type Level string
 
-// Define constant app levels.
+// Define constant app Levels.
 const (
-	ErrorL   level = "error"
-	SuccessL level = "success"
-	InfoL    level = "info"
+	ErrorL   Level = "error"
+	SuccessL Level = "success"
+	InfoL    Level = "info"
 )
 
 // Define constant color codes.
@@ -38,37 +38,45 @@ const (
 	NOCOLOR string = "\033[0m"
 )
 
-// Alert, prints message at given [level].
+// Define constant icon/title codes.
+const (
+	ERROR   string = "[ERROR]"
+	SUCCESS string = "[OK]"
+	INFO    string = "[INFO]"
+)
+
+// Alert, prints message at given [Level].
 //
-// l - (level) decides style(level) of log message.
+// l - (Level) decides style(Level) of log message.
 // msg - (message) is the content of log message.
-func Alert(l level, msg string) {
+func Alert(l Level, msg string) {
 	// Configure message
-	message := fmt.Sprintf("\n %s %s \n", outputLevel(l), msg)
+	message := fmt.Sprintf("\n %s %s \n", OutputLevel(l), msg)
 
 	fmt.Fprintln(Stdout, message)
 }
 
-// outputLevel sets [Color] and [Icon] by given `level`,
-// and then, returns final printable level title.
+// OutputLevel sets [Color] and [Icon] by given `Level`,
+// and then, returns final printable Level title.
 //
 // Result cases:
 // [ERROR] - (powered with red color)
 // [OK] - (powered with green color)
 // [INFO] - (powered with yellow color)
-func outputLevel(l level) string {
+func OutputLevel(l Level) string {
 	switch l {
 	case ErrorL:
 		Color = RED
-		Icon = "[ERROR]"
+		Icon = ERROR
 	case SuccessL:
 		Color = GREEN
-		Icon = "[OK]"
+		Icon = SUCCESS
 	case InfoL:
 		Color = YELLOW
-		Icon = "[INFO]"
+		Icon = INFO
 	default:
 		Color = NOCOLOR
+		Icon = ""
 	}
 
 	return fmt.Sprintf("%s%s%s", Color, Icon, NOCOLOR)
