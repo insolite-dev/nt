69454adb786abec55d1807d32d8684b3b085974b
Ismael Shakverdiev <st.bahahd@gmail.com>
2021-12-07 15:54:27 +0400

    Add level utilities of logging content
    - Added three constant level type - [error, success, info]
    - Added constant color codes - [red, green, yellow, no-color(default)]
    - Added output level function to get final printable level title, by
      early created constant level.

--- pkg/logger.go
+++ pkg/logger.go
@@ -1,7 +1,60 @@
+// Copyright 2021-present Anon. All rights reserved.
+// Use of this source code is governed by Apache 2.0 license
+// that can be found in the LICENSE file.
+
 package pkg
 
-/* TODO:
-- Add colorized error logger
-- Add colorized success logger
-- Add loading indicator logger
-*/
+import (
+	"fmt"
+)
+
+// Color and Icon of "CURRENT" output message.
+var (
+	Color string
+	Icon  string
+)
+
+// level is a custom type of `string-level`.
+// used define level for [setOutputStyle] function.
+type level string
+
+// Define constant app levels.
+const (
+	errorL   level = "error"
+	successL level = "success"
+	infoL    level = "info"
+)
+
+// Define constant color codes.
+const (
+	RED     string = "\033[0;31m"
+	GREEN   string = "\033[0;32m"
+	YELLOW  string = "\033[1;33m"
+	NOCOLOR string = "\033[0m"
+)
+
+// outputLevel sets [Color] and [Icon] by given `level`,
+// and then, returns final printable level title.
+//
+// Result cases:
+// [ERROR] - (powered with red color)
+// [SUCCESS] - (powered with green color)
+// [INFO] - (powered with yellow color)
+func outputLevel(l level) string {
+	switch l {
+	case errorL:
+		Color = RED
+		Icon = "[ERROR]"
+	case successL:
+		Color = GREEN
+		Icon = "[OK]"
+		break
+	case infoL:
+		Color = YELLOW
+		Icon = "[INFO]"
+	default:
+		Color = NOCOLOR
+	}
+
+	return fmt.Sprintf("%s%s%s", Color, Icon, NOCOLOR)
+}
--- pkg/utils.go
+++
@@ -1,8 +0,0 @@
-package pkg
-
-import "github.com/mattn/go-colorable"
-
-var (
-	Stdout = colorable.NewColorableStdout() // add a colorable std out
-	Stderr = colorable.NewColorableStderr() // add a colorable std err
-)
