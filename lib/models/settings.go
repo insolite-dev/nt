fd0a240d3207c129e73c22d6d3cd9625d344c4e8
Ismael Shakverdiev <st.bahahd@gmail.com>
2022-01-08 19:18:33 +0400

    feature: add setting's initializing functionality
    - Added setting's initializing functionality to application's
      initialization function (local service's init function).
    - Added FromJSON functionality to settings value, to convert
      json(as-string) to settings structure model.

--- lib/models/settings.go
+++ lib/models/settings.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"encoding/json"
+
+	"github.com/mitchellh/mapstructure"
+)
+
 // All editors listed.
 var (
 	// VI related.
@@ -22,4 +28,28 @@ type Settings struct {
 // InitSettings returns default variant of settings structure model.
 func InitSettings() Settings {
 	return Settings{Editor: VI}
+}
+
+// ToByte converts settings model to JSON map,
+// but returns that value as byte array.
+func (s *Settings) ToByte() []byte {
+	b, _ := json.Marshal(&s)
+
+	var j map[string]interface{}
+	_ = json.Unmarshal(b, &j)
+
+	res, _ := json.Marshal(&j)
+
+	return res
+}
+
+// FromJSON converts string(map) value to Settings structure.
+func FromJSON(value string) Settings {
+	var m map[string]interface{}
+	_ = json.Unmarshal([]byte(value), &m)
+
+	var s Settings
+	mapstructure.Decode(m, &s)
+
+	return s
 }
