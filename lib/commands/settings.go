71e454c958a509377a20f0075e417f1995a46640
Ismael Shakverdiev <st.bahahd@gmail.com>
2022-01-27 15:21:53 +0400

    feature: fix typo on settings command

--- lib/commands/settings.go
+++ lib/commands/settings.go
@@ -1,4 +1,4 @@
-	// Copyright 2021-present Anon. All rights reserved.
+// Copyright 2021-present Anon. All rights reserved.
 // Use of this source code is governed by Apache 2.0 license
 // that can be found in the LICENSE file.
 
