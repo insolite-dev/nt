a48225b299270ac2370c096573dc857c7c8fcad9
Ismael Shakverdiev <st.bahahd@gmail.com>
2022-02-14 10:17:01 +0400

    Revert "feature: rename command arguments"
    This reverts commit c13846daab359485d7817224e5becf9a5d4cdc99.

--- lib/commands/rename.go
+++ lib/commands/rename.go
@@ -28,18 +28,10 @@ func initRenameCommand() {
 // runRenameCommand runs appropriate service commands to rename note.
 func runRenameCommand(cmd *cobra.Command, args []string) {
 	// Take note title from arguments. If it's provided.
-	if len(args) == 2 {
-		editableNote := models.EditNote{
-			Current : models.Note{Title: args[0]},
-			New: models.Note{Title: args[1]},
-		}
+	if len(args) > 0 {
+		note := models.Note{Title: args[0]}
 
-		rename(editableNote.Current.Title, editableNote.New.Title)
-		return
-	} 
-
-	if len(args) == 1 {
-		askAndRename(args[0])
+		askAndRename(note.Title)
 		return
 	}
 
@@ -66,21 +58,6 @@ func askAndRename(selected string) {
 	var newname string
 	survey.AskOne(assets.NewNamePrompt(selected), &newname)
 
-	// Generate editable note by current note and updated note.
-	editableNote := models.EditNote{
-		Current: models.Note{Title: selected},
-		New:     models.Note{Title: newname},
-	}
-
-	if _, err := service.Rename(editableNote); err != nil {
-		pkg.Alert(pkg.ErrorL, err.Error())
-		return
-	}
-
-	pkg.Alert(pkg.SuccessL, "Note renamed successfully: "+newname)
-}
-
-func rename(selected string, newname string) {
 	// Generate editable note by current note and updated note.
 	editableNote := models.EditNote{
 		Current: models.Note{Title: selected},
