e90beb8c011279320fe03b4e4d3b5b34bc3a94cc
Ismael Shakverdiev <st.bahahd@gmail.com>
2022-02-18 22:51:05 +0400

    feature: clean-up rename command

--- lib/commands/rename.go
+++ lib/commands/rename.go
@@ -5,8 +5,6 @@
 package commands
 
 import (
-	"fmt"
-
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/anonistas/notya/assets"
 	"github.com/anonistas/notya/lib/models"
@@ -17,7 +15,7 @@ import (
 // renameCommand is a command model which used to change name of notes or files.
 var renameCommand = &cobra.Command{
 	Use:     "rename",
-	Aliases: []string{"rn"},
+	Aliases: []string{"rn", "mv"},
 	Short:   "Change/Update note's name",
 	Run:     runRenameCommand,
 }
@@ -29,23 +27,24 @@ func initRenameCommand() {
 
 // runRenameCommand runs appropriate service commands to rename note.
 func runRenameCommand(cmd *cobra.Command, args []string) {
-	// Take note title from arguments. If it's provided.
+	// Use arguments for old and new note names.
 	if len(args) == 2 {
 		editableNote := models.EditNote{
-			Current : models.Note{Title: args[0]},
-			New: models.Note{Title: args[1]},
+			Current: models.Note{Title: args[0]},
+			New:     models.Note{Title: args[1]},
 		}
 
 		rename(editableNote.Current.Title, editableNote.New.Title)
 		return
-	} 
+	}
 
+	// Use first argument for old note name.
 	if len(args) == 1 {
 		askAndRename(args[0])
 		return
 	}
 
-	// Generate array of all note names.
+	// Generate array of all note names, if arguments is empty.
 	notes, err := service.GetAll()
 	if err != nil {
 		pkg.Alert(pkg.ErrorL, err.Error())
@@ -55,7 +54,7 @@ func runRenameCommand(cmd *cobra.Command, args []string) {
 	// Ask for note selection.
 	var selected string
 	survey.AskOne(
-		assets.ChooseNotePrompt("rename", pkg.MapNotesList(notes)),
+		assets.ChooseNotePrompt("rename:", pkg.MapNotesList(notes)),
 		&selected,
 	)
 
@@ -68,18 +67,7 @@ func askAndRename(selected string) {
 	var newname string
 	survey.AskOne(assets.NewNamePrompt(selected), &newname)
 
-	// Generate editable note by current note and updated note.
-	editableNote := models.EditNote{
-		Current: models.Note{Title: selected},
-		New:     models.Note{Title: newname},
-	}
-
-	if _, err := service.Rename(editableNote); err != nil {
-		pkg.Alert(pkg.ErrorL, err.Error())
-		return
-	}
-
-	pkg.Alert(pkg.SuccessL, fmt.Sprintf("Note renamed: `%v` to `%v`", selected, newname))
+	rename(selected, newname)
 }
 
 func rename(selected string, newname string) {
