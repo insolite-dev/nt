83240afec96ff04e28a14c365ce9fc0c8575e114
Ismael Shakverdiev <st.bahahd@gmail.com>
2022-04-02 22:26:07 +0400

    feature: add folder structure model

---
+++ lib/models/folder.go
@@ -0,0 +1,33 @@
+// Copyright 2022-present Anon. All rights reserved.
+// Use of this source code is governed by Apache 2.0 license
+// that can be found in the LICENSE file.
+
+package models
+
+// Folder is a w-directory representation data structure.
+//
+//  EXAMPLE:
+// ╭───────────────╮
+// │ ~/notya-path/ │
+// │───────────────╯
+// │─ new_note.txt
+// │─ todo/   ◀──── Sub directory of main notes folder.
+// │  │── today.md
+// │  │── tomorrow.md
+// │  ╰── anon-notya/  ◀── Sub directory of "todo" folder.
+// │      │── issues.txt
+// │      ╰── features.txt
+// │─ ted-talks.tx
+// ╰─ psycology_resources.txt
+//
+type Folder struct {
+	// Title is the name(not path) of "current" folder.
+	Title string `json:"title"`
+
+	// Path is the full-path string name of "current" folder.
+	Path string `json:"path"`
+
+	// Files is the slice of "current" folder's sub-nodes.
+	// Includes full-path strings of the files/folders names.
+	Files []string `json:"files"`
+}
