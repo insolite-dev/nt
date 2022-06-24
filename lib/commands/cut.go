a40e908928fe6bbe9fbb3f95f50b3187ade5c78c
Ismael Shakverdiev <theisk44@gmail.com>
2022-06-24 15:18:08 +0400

    feat: add "cut" command

--- lib/commands/cut.go
+++ lib/commands/cut.go
@@ -12,25 +12,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// copyCommand is a command model which used to copy files.
-var copyCommand = &cobra.Command{
-	Use:     "copy",
-	Aliases: []string{"c"},
-	Short:   "Copy file's body to clipboard",
-	Run:     runCopyCommand,
+var cutCommand = &cobra.Command{
+	Use:   "cut",
+	Short: "Cut the file | copies the file and saves it data to clipboard",
+	Run:   runCutCommand,
 }
 
-// initCopyCommand initializes copyCommand to the main application command.
-func initCopyCommand() {
-	appCommand.AddCommand(copyCommand)
+func initCutCommand() {
+	appCommand.AddCommand(cutCommand)
 }
 
-// runCopyCommand runs appropriate service commands to copy note data to clipboard.
-func runCopyCommand(cmd *cobra.Command, args []string) {
+// runCutCommand runs appropriate service commands to cut the note file.
+func runCutCommand(cmd *cobra.Command, args []string) {
 	determineService()
 
 	if len(args) > 0 {
-		copyAndFinish(models.Note{Title: args[0]})
+		cutAndFinish(models.Note{Title: args[0]})
 		return
 	}
 
@@ -46,21 +43,19 @@ func runCopyCommand(cmd *cobra.Command, args []string) {
 	// Ask for node selection.
 	var selected string
 	survey.AskOne(
-		assets.ChooseNodePrompt("note", "copy", nodeNames),
+		assets.ChooseNodePrompt("note", "cut", nodeNames),
 		&selected,
 	)
 
-	copyAndFinish(models.Note{Title: selected})
+	cutAndFinish(models.Note{Title: selected})
 }
 
-func copyAndFinish(note models.Note) {
+func cutAndFinish(note models.Note) {
 	loading.Start()
-	if err := service.Copy(note); err != nil {
+	if _, err := service.Cut(note); err != nil {
 		loading.Stop()
 		pkg.Alert(pkg.ErrorL, err.Error())
 		return
 	}
-
 	loading.Stop()
-	pkg.Alert(pkg.SuccessL, "Note copied to clipboard")
 }
